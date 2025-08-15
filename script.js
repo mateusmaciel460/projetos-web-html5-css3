771a78c6724ad1ea1f2aa792ab92b9b7bbae77b4
mateusmaciel460 <mateusmaciel460@gmail.com>
2025-08-15 14:24:46 -0300

    adicionando projeto-portfolio na lista de projetos

---
+++ script.js
@@ -0,0 +1,32 @@
+const elementoProjetos = document.querySelector('#elemento-projetos');
+
+const listaProjetos = [
+    { 
+        id: 1, 
+        titulo: 'Projeto Portfolio', 
+        tag: 'portfolio', 
+        linkImagem: 'yBSr81DJ/logo-portfolio.png' 
+    }
+];
+
+function exibirListaProjetosNaTela() {
+    listaProjetos.forEach((projeto) => {
+        elementoProjetos.innerHTML += `
+            <div class="projeto modelo__vertical modelo__centralizado">
+                <h2 class="conteudo__subtitulo">${projeto.titulo}</h2>
+
+                <span class="conteudo__tag modelo__cor-verde">
+                    ${projeto.tag}
+                </span>
+
+                <img src="https://i.ibb.co/${projeto.linkImagem}" alt="Logo - ${projeto.titulo}"/>
+
+                <a href="${projeto.tag}/index.html" target="_blank" class="conteudo__botao modelo__cor-azul">
+                    Acessar
+                </a>
+            </div>
+        `;
+    });
+}
+
+exibirListaProjetosNaTela();
